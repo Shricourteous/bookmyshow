fa302d5acb22b3f7be31672a4ad88b518f2fb9b2
Shri Balaji <75971598+Shricourteous@users.noreply.github.com>
2022-09-26 14:59:42 -1000

    Getting ready to deploy

--- src/components/PlayFilter/PlayFilters.component.jsx
+++ src/components/PlayFilter/PlayFilters.component.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Disclosure } from '@headlessui/react';
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
 
-const PlayFilters = () => {
-  return (
-    <div>PlayFilters</div>
-  )
+const PlayFilters = (props) => {
+  return (<Disclosure>
+    {({ open }) => (
+      <>
+        <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+          <span className={open? "text-rose-600" : "text-gray-700"}>{props.title}</span>
+          {!open?<BiChevronDown/>:<BiChevronUp/>}
+        </Disclosure.Button>
+        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+          <div className='flex item-center gap-3 flex-wrap'>
+            {props.tags.map((tag, index)=>(
+            <div key={index} className='border-2 border-gray-200 px-3 py-2 rounded'>
+              <span className='text-red-600'>{tag}</span>
+            </div>
+          ))}
+          </div>
+        </Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>)
 }
 
 export default PlayFilters;
--- src/pages/Play.Page.jsx
+++ src/pages/Play.Page.jsx
@@ -8,16 +8,44 @@ const PlayPage = () => {
   return (
     <>
       <div className='container mx-auto px-4¡ my-10'>
-        <div className='w-full lg:flex lg:flex-row-reverse gap-4'>
-          <div className='lg:w-3/4 p-4 bg-white rounded'>
-            <h2 className='text-2xl font-bold mb-4'>Play in NCR TamilNadu</h2>
+        <div className='w-full flex flex-col-reverse lg:flex lg:flex-row-reverse gap-4'>
+          <div className='lg:w-3/4 p-4 bg-slate-400 rounded'>
+            <h2 className='text-2xl font-bold mb-4'>Play in CBE TamilNadu</h2>
             <div className='flex flex-wrap '>
+            <div className='w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center'>
+              <Poster isPlay= {true} src='https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ara-gora-comedy-open-mic-nights-0-2022-9-11-t-10-17-18.jpg' title='Open Mic Nights' subtitle='Lets Talk..' />                
+              </div>
               <div className='w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center'>
-                <Poster isPlay= {true} src='https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ara-gora-comedy-open-mic-nights-0-2022-9-11-t-10-17-18.jpg' title='Open Mic Nights' subtitle='Lets Talk..' />                
+              <Poster isPlay= {true} src='https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ara-gora-comedy-open-mic-nights-0-2022-9-11-t-10-17-18.jpg' title='Open Mic Nights' subtitle='Lets Talk..' />                
+              </div>
+              <div className='w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center'>
+              <Poster isPlay= {true} src='https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ara-gora-comedy-open-mic-nights-0-2022-9-11-t-10-17-18.jpg' title='Open Mic Nights' subtitle='Lets Talk..' />                
+              </div>
+              <div className='w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center'>
+              <Poster isPlay= {true} src='https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ara-gora-comedy-open-mic-nights-0-2022-9-11-t-10-17-18.jpg' title='Open Mic Nights' subtitle='Lets Talk..' />                
+              </div>
+              <div className='w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center'>
+              <Poster isPlay= {true} src='https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ara-gora-comedy-open-mic-nights-0-2022-9-11-t-10-17-18.jpg' title='Open Mic Nights' subtitle='Lets Talk..' />                
+              </div>
+              <div className='w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center'>
+              <Poster isPlay= {true} src='https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ara-gora-comedy-open-mic-nights-0-2022-9-11-t-10-17-18.jpg' title='Open Mic Nights' subtitle='Lets Talk..' />                
               </div>
             </div>
           </div>
+          <div className='lg:w-1/4 p-4 bg-white rounded'>
+            <h2 className='text-2xl font-bold mb-4'>
+              Filters
+            </h2>
+            <div>
+              <PlayFilters title="Date" tags={["Today","Tomorrow", "This weekend"]}/>
+            </div>
+            <div>
+              <PlayFilters title="Language" tags={["Tamil","Hindi", "Russian"]}/>
+            </div>
+
+          </div>
         </div>
+        <iframe title='Play Location' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15663.868121193647!2d76.91399302667243!3d11.041098774275035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba858b6c24ba54d%3A0x84e6712fe7fc5069!2sEdayarpalayam%2C%20Coimbatore%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1664239210095!5m2!1sen!2sin" width="600" height="450" style={{border:"0"}} allowFulScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </>
   );
